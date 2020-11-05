eb84f0d39604f5450f67f1ac5279745732a31503
Thomas Judge <Judgemaster@gmail.com>
2020-11-04 20:55:35 -0500

    still fixing stuff

---
+++ Acronym Forge 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AcronymForge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a multiword term to be hammered into an acronym:");
+            string input  = Console.ReadLine();
+            Array.ForEach(input.Split(' '), s => Console.Write(s[0] + "."));
+            Console.WriteLine();
+        }
+    }
+}
